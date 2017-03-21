9775c9a178de0c2866e91737692ef691b49e917a
James Munnelly <james.munnelly@jetstack.io>
2017-03-21 16:11:03 +0000

    Automatically create ElasticsearchCluster ThirdPartyResource type

---
+++ pkg/tpr/tpr.go
@@ -0,0 +1,36 @@
+package tpr
+
+import (
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/apis/extensions/v1beta1"
+
+	"gitlab.jetstack.net/marshal/colonel/pkg/api/v1"
+)
+
+var thirdPartyResource = &v1beta1.ThirdPartyResource{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "elasticsearch-cluster." + v1.GroupName,
+	},
+	Description: "A specification of an Elasticsearch cluster",
+	Versions: []v1beta1.APIVersion{
+		{
+			Name: "v1",
+		},
+	},
+}
+
+// Ensure will ensure that the appropriate ThirdPartyResources exist in
+// the target Kubernetes cluster
+func Ensure(cl *kubernetes.Clientset) error {
+	_, err := cl.Extensions().ThirdPartyResources().Create(thirdPartyResource)
+
+	if err != nil {
+		if !errors.IsAlreadyExists(err) {
+			return err
+		}
+	}
+
+	return nil
+}
