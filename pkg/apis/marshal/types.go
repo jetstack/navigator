37c383e92e9c16344e8eb780083a8673064c69c8
James Munnelly <james.munnelly@jetstack.io>
2017-05-11 12:21:38 +0100

    Remove unneccessary methods

--- pkg/apis/marshal/types.go
+++ pkg/apis/marshal/types.go
@@ -2,7 +2,6 @@ package marshal
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/pkg/api/v1"
 )
 
@@ -33,26 +32,6 @@ type ElasticsearchClusterList struct {
 	metav1.ListMeta
 
 	Items []ElasticsearchCluster
-}
-
-// Required to satisfy Object interface
-func (e *ElasticsearchCluster) GetObjectKind() schema.ObjectKind {
-	return &e.TypeMeta
-}
-
-// Required to satisfy ObjectMetaAccessor interface
-func (e *ElasticsearchCluster) GetObjectMeta() metav1.Object {
-	return &e.ObjectMeta
-}
-
-// Required to satisfy Object interface
-func (el *ElasticsearchClusterList) GetObjectKind() schema.ObjectKind {
-	return &el.TypeMeta
-}
-
-// Required to satisfy ListMetaAccessor interface
-func (el *ElasticsearchClusterList) GetListMeta() metav1.List {
-	return &el.ListMeta
 }
 
 type ElasticsearchClusterSpec struct {
