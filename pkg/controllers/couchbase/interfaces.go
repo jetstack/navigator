dadb5059e5773f4389fb4db2c50a6c54ad6ced72
Richard Wall <richard.wall@jetstack.io>
2017-08-29 14:03:43 +0100

    Remove unused interfaces

--- pkg/controllers/couchbase/interfaces.go
+++ pkg/controllers/couchbase/interfaces.go
@@ -5,22 +5,3 @@ import "github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
 type CouchbaseClusterControl interface {
 	SyncCouchbaseCluster(v1alpha1.CouchbaseCluster) error
 }
-
-type CouchbaseClusterNodePoolControl interface {
-	CreateElasticsearchClusterNodePool(v1alpha1.ElasticsearchCluster, v1alpha1.CouchbaseClusterNodePool) error
-	UpdateElasticsearchClusterNodePool(v1alpha1.ElasticsearchCluster, v1alpha1.CouchbaseClusterNodePool) error
-	DeleteElasticsearchClusterNodePool(v1alpha1.ElasticsearchCluster, v1alpha1.CouchbaseClusterNodePool) error
-}
-
-type CouchbaseClusterServiceAccountControl interface {
-	CreateElasticsearchClusterServiceAccount(v1alpha1.CouchbaseCluster) error
-	UpdateElasticsearchClusterServiceAccount(v1alpha1.CouchbaseCluster) error
-	DeleteElasticsearchClusterServiceAccount(v1alpha1.CouchbaseCluster) error
-}
-
-type CouchbaseClusterServiceControl interface {
-	CreateElasticsearchClusterService(v1alpha1.CouchbaseCluster) error
-	UpdateElasticsearchClusterService(v1alpha1.CouchbaseCluster) error
-	DeleteElasticsearchClusterService(v1alpha1.CouchbaseCluster) error
-	NameSuffix() string
-}
