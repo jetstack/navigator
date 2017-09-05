2bab6b0768e6e4ebc68487196a10a04d5dbaaa4f
James Munnelly <james.munnelly@jetstack.io>
2017-09-05 08:57:42 +0100

    Add QueueingEventHandler and BlockingEventHandler types

--- pkg/controllers/util.go
+++ pkg/controllers/util.go
@@ -1,7 +1,64 @@
 package controllers
 
-import "k8s.io/client-go/tools/cache"
+import (
+	"reflect"
+
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
 
 var (
 	KeyFunc = cache.DeletionHandlingMetaNamespaceKeyFunc
 )
+
+type QueuingEventHandler struct {
+	Queue workqueue.RateLimitingInterface
+}
+
+func (q *QueuingEventHandler) Enqueue(obj interface{}) {
+	key, err := KeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	q.Queue.Add(key)
+}
+
+func (q *QueuingEventHandler) OnAdd(obj interface{}) {
+	q.Enqueue(obj)
+}
+
+func (q *QueuingEventHandler) OnUpdate(old, new interface{}) {
+	if reflect.DeepEqual(old, new) {
+		return
+	}
+	q.Enqueue(new)
+}
+
+func (q *QueuingEventHandler) OnDelete(obj interface{}) {
+	q.Enqueue(obj)
+}
+
+type BlockingEventHandler struct {
+	WorkFunc func(obj interface{})
+}
+
+func (b *BlockingEventHandler) Enqueue(obj interface{}) {
+	b.WorkFunc(obj)
+}
+
+func (b *BlockingEventHandler) OnAdd(obj interface{}) {
+	b.WorkFunc(obj)
+}
+
+func (b *BlockingEventHandler) OnUpdate(old, new interface{}) {
+	if reflect.DeepEqual(old, new) {
+		return
+	}
+	b.WorkFunc(new)
+}
+
+func (b *BlockingEventHandler) OnDelete(obj interface{}) {
+	b.WorkFunc(obj)
+}
