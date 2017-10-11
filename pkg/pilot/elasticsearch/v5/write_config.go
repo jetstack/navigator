e2d448b8a7149412f6c5051ecb17fd55b1ca3819
James Munnelly <james.munnelly@jetstack.io>
2017-10-11 22:31:31 +0100

    Add log messages to WriteConfig hook

--- pkg/pilot/elasticsearch/v5/write_config.go
+++ pkg/pilot/elasticsearch/v5/write_config.go
@@ -7,6 +7,8 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/golang/glog"
+
 	"github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
 )
 
@@ -17,21 +19,25 @@ const (
 func (p *Pilot) WriteConfig(pilot *v1alpha1.Pilot) error {
 	esConfigPath := fmt.Sprintf("%s/%s", p.Options.ConfigDir, elasticsearchConfigSubDir)
 	err := filepath.Walk(esConfigPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		glog.V(4).Infof("Considering file %q when writing elasticsearch config", path)
 		if info.IsDir() {
+			glog.V(4).Infof("%q is a directory", path)
 			if strings.HasPrefix(info.Name(), "..") {
 				return filepath.SkipDir
 			}
 			return nil
-		}
-		if err != nil {
-			return err
 		}
 		relPath, err := filepath.Rel(esConfigPath, path)
 		if err != nil {
 			return err
 		}
 		dstPath := fmt.Sprintf("%s/%s", p.Options.ElasticsearchOptions.ConfigDir, relPath)
+		glog.V(4).Infof("Relative destination path %q, destination path %q")
 		relDir := filepath.Dir(relPath)
+		glog.V(4).Infof("Ensuring directory %q exists")
 		stat, err := os.Stat(relDir)
 		if os.IsNotExist(err) {
 			err = os.MkdirAll(relDir, 0644)
