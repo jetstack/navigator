b6b8aaebb045138b394361446b2e52630b5adce8
Richard Wall <richard.wall@jetstack.io>
2017-10-18 17:49:23 +0100

    Create a test fixture

--- pkg/controllers/cassandra/cluster_control_test.go
+++ pkg/controllers/cassandra/cluster_control_test.go
@@ -4,29 +4,35 @@ import (
 	"testing"
 
 	"github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
+	navigatorclientset "github.com/jetstack-experimental/navigator/pkg/client/clientset_generated/clientset"
 	"github.com/jetstack-experimental/navigator/pkg/client/clientset_generated/clientset/fake"
 	"github.com/jetstack-experimental/navigator/pkg/controllers/cassandra"
 	"github.com/stretchr/testify/assert"
 )
 
-func cluster(t *testing.T) *v1alpha1.CassandraCluster {
+type fixture struct {
+	cluster *v1alpha1.CassandraCluster
+	client  navigatorclientset.Interface
+	control cassandra.ControlInterface
+}
+
+func setup(t *testing.T) *fixture {
 	namespace := "foo"
 	name := "bar"
 	cluster := &v1alpha1.CassandraCluster{}
 	cluster.SetName(name)
 	cluster.SetNamespace(namespace)
-	return cluster
+	client := fake.NewSimpleClientset(cluster)
+	control := cassandra.NewController(client)
+	return &fixture{cluster, client, control}
 }
 
 func TestControl(t *testing.T) {
-	client := fake.NewSimpleClientset()
-	c := cassandra.NewController(client)
-
 	t.Run(
 		"new cluster",
 		func(t *testing.T) {
-			cluster := cluster(t)
-			err := c.Sync(cluster)
+			fixture := setup(t)
+			err := fixture.control.Sync(fixture.cluster)
 			assert.NoError(t, err)
 		},
 	)
