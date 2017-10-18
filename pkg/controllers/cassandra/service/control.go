70fa1aa248a41b390c6abbef23fa70e3f9c30392
Richard Wall <richard.wall@jetstack.io>
2017-10-18 23:05:28 +0100

    unfinished supplying service control with a service lister....

--- pkg/controllers/cassandra/service/control.go
+++ pkg/controllers/cassandra/service/control.go
@@ -1,9 +1,9 @@
 package service
 
 import (
-	"k8s.io/client-go/kubernetes"
-
 	v1alpha1 "github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
+	"k8s.io/client-go/kubernetes"
+	corelisters "k8s.io/client-go/listers/core/v1"
 )
 
 type Interface interface {
@@ -11,16 +11,19 @@ type Interface interface {
 }
 
 type defaultCassandraClusterServiceControl struct {
-	kubeClient kubernetes.Interface
+	kubeClient    kubernetes.Interface
+	serviceLister corelisters.ServiceLister
 }
 
 var _ Interface = &defaultCassandraClusterServiceControl{}
 
 func NewControl(
 	kubeClient kubernetes.Interface,
+	serviceLister corelisters.ServiceLister,
 ) Interface {
 	return &defaultCassandraClusterServiceControl{
-		kubeClient: kubeClient,
+		kubeClient:    kubeClient,
+		serviceLister: serviceLister,
 	}
 }
 
--- pkg/controllers/cassandra/service/control_test.go
+++ pkg/controllers/cassandra/service/control_test.go
@@ -3,6 +3,7 @@ package service_test
 import (
 	"testing"
 
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes/fake"
 
 	"github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
@@ -15,7 +16,9 @@ func TestServiceControl(t *testing.T) {
 	cluster.SetNamespace("foo")
 	cluster.SetName("bar")
 	kclient := fake.NewSimpleClientset()
-	c := service.NewControl(kclient)
+	factory := informers.NewSharedInformerFactory(kclient, 0)
+	serviceLister := factory.Core().V1().Services().Lister()
+	c := service.NewControl(kclient, serviceLister)
 	err := c.Sync(cluster)
 	assert.NoError(t, err)
 }
