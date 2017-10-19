64d33a3921560b12546de911c486832f6d18c1d3
Richard Wall <richard.wall@jetstack.io>
2017-10-19 16:59:29 +0100

    Tests for creating service and resync of service

--- pkg/controllers/cassandra/service/control.go
+++ pkg/controllers/cassandra/service/control.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	v1alpha1 "github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
+	apiv1 "k8s.io/api/core/v1"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/kubernetes"
 	corelisters "k8s.io/client-go/listers/core/v1"
 )
@@ -27,6 +29,14 @@ func NewControl(
 	}
 }
 
-func (e *defaultCassandraClusterServiceControl) Sync(c *v1alpha1.CassandraCluster) error {
-	return nil
+func (e *defaultCassandraClusterServiceControl) Sync(cluster *v1alpha1.CassandraCluster) error {
+	svc := &apiv1.Service{}
+	svc.SetNamespace(cluster.Namespace)
+	svc.SetName(cluster.Name + "-service")
+
+	_, err := e.kubeClient.CoreV1().Services(svc.Namespace).Create(svc)
+	if k8sErrors.IsAlreadyExists(err) {
+		return nil
+	}
+	return err
 }
--- pkg/controllers/cassandra/service/control_test.go
+++ pkg/controllers/cassandra/service/control_test.go
@@ -3,22 +3,79 @@ package service_test
 import (
 	"testing"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes/fake"
 
 	"github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
 	"github.com/jetstack-experimental/navigator/pkg/controllers/cassandra/service"
-	"github.com/stretchr/testify/assert"
 )
 
-func TestServiceControl(t *testing.T) {
-	cluster := &v1alpha1.CassandraCluster{}
-	cluster.SetNamespace("foo")
-	cluster.SetName("bar")
+type fixture struct {
+	t       *testing.T
+	control service.Interface
+	kclient *fake.Clientset
+}
+
+func newFixture(t *testing.T) *fixture {
 	kclient := fake.NewSimpleClientset()
 	factory := informers.NewSharedInformerFactory(kclient, 0)
 	serviceLister := factory.Core().V1().Services().Lister()
-	c := service.NewControl(kclient, serviceLister)
-	err := c.Sync(cluster)
-	assert.NoError(t, err)
+	control := service.NewControl(kclient, serviceLister)
+
+	return &fixture{
+		t:       t,
+		control: control,
+		kclient: kclient,
+	}
+}
+
+func (f *fixture) run(cluster *v1alpha1.CassandraCluster) {
+	err := f.control.Sync(cluster)
+	if err != nil {
+		f.t.Error(err)
+	}
+}
+
+func (f *fixture) expectService(namespace, name string) {
+	_, err := f.kclient.CoreV1().Services(namespace).Get(
+		name,
+		metav1.GetOptions{},
+	)
+	if err != nil {
+		f.t.Log("Actions:")
+		f.t.Log(f.kclient.Actions())
+		f.t.Error(err)
+	}
+}
+
+func newCassandraCluster() *v1alpha1.CassandraCluster {
+	c := &v1alpha1.CassandraCluster{}
+	c.SetNamespace("foo")
+	c.SetName("bar")
+	return c
+}
+
+func TestServiceControl(t *testing.T) {
+	t.Run(
+		"service created",
+		func(t *testing.T) {
+			cluster := newCassandraCluster()
+			f := newFixture(t)
+			f.run(cluster)
+			f.expectService(cluster.Namespace, cluster.Name+"-service")
+		},
+	)
+	t.Run(
+		"resync",
+		func(t *testing.T) {
+			cluster := newCassandraCluster()
+			f := newFixture(t)
+			f.run(cluster)
+			f.expectService(cluster.Namespace, cluster.Name+"-service")
+			f.run(cluster)
+			f.expectService(cluster.Namespace, cluster.Name+"-service")
+		},
+	)
 }
