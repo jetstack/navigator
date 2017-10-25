6225d5a79afe11f40a37ec0479321984a8650bb3
Richard Wall <richard.wall@jetstack.io>
2017-10-25 12:12:03 +0100

    Remove some unused functions

--- pkg/controllers/cassandra/util/util.go
+++ pkg/controllers/cassandra/util/util.go
@@ -2,9 +2,7 @@ package util
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/selection"
 
 	"github.com/jetstack-experimental/navigator/pkg/apis/navigator"
 	v1alpha1 "github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
@@ -19,18 +17,6 @@ const (
 	ClusterNameLabelKey = "navigator.jetstack.io/cassandra-cluster-name"
 )
 
-func Int32Ptr(i int32) *int32 {
-	return &i
-}
-
-func Int64Ptr(i int64) *int64 {
-	return &i
-}
-
-func BoolPtr(b bool) *bool {
-	return &b
-}
-
 func NewControllerRef(c *v1alpha1.CassandraCluster) metav1.OwnerReference {
 	return *metav1.NewControllerRef(c, schema.GroupVersionKind{
 		Group:   navigator.GroupName,
@@ -41,18 +27,6 @@ func NewControllerRef(c *v1alpha1.CassandraCluster) metav1.OwnerReference {
 
 func ResourceBaseName(c *v1alpha1.CassandraCluster) string {
 	return typeName + "-" + c.Name
-}
-
-func SelectorForCluster(c *v1alpha1.CassandraCluster) (labels.Selector, error) {
-	clusterNameReq, err := labels.NewRequirement(
-		ClusterNameLabelKey,
-		selection.Equals,
-		[]string{c.Name},
-	)
-	if err != nil {
-		return nil, err
-	}
-	return labels.NewSelector().Add(*clusterNameReq), nil
 }
 
 func ClusterLabels(c *v1alpha1.CassandraCluster) map[string]string {
