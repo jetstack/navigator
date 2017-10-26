0c04871ff6af96308b99afa1b8442988f39c0cde
Richard Wall <richard.wall@jetstack.io>
2017-10-26 10:19:25 +0100

    Begin adding pod template and hash annotation

--- pkg/controllers/cassandra/nodepool/resource.go
+++ pkg/controllers/cassandra/nodepool/resource.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jetstack-experimental/navigator/pkg/apis/navigator/v1alpha1"
 	"github.com/jetstack-experimental/navigator/pkg/controllers/cassandra/util"
 	apps "k8s.io/api/apps/v1beta2"
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -13,7 +14,7 @@ func StatefulSetForCluster(
 ) *apps.StatefulSet {
 
 	statefulSetName := util.NodePoolResourceName(cluster, np)
-
+	nodePoolLabels := util.NodePoolLabels(cluster, np.Name)
 	set := &apps.StatefulSet{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:            statefulSetName,
@@ -26,13 +27,22 @@ func StatefulSetForCluster(
 			Replicas:    util.Int32Ptr(int32(np.Replicas)),
 			ServiceName: statefulSetName,
 			Selector: &metav1.LabelSelector{
-				MatchLabels: util.NodePoolLabels(cluster, np.Name),
+				MatchLabels: nodePoolLabels,
 			},
 			UpdateStrategy: apps.StatefulSetUpdateStrategy{
 				Type: apps.RollingUpdateStatefulSetStrategyType,
 			},
 			PodManagementPolicy: apps.ParallelPodManagement,
-			// Template:            *elasticsearchPodTemplate,
+			Template: apiv1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: nodePoolLabels,
+					Annotations: map[string]string{
+						util.NodePoolHashAnnotationKey: util.ComputeNodePoolHash(
+							cluster, np, util.Int32Ptr(0),
+						),
+					},
+				},
+			},
 		},
 	}
 	return set
