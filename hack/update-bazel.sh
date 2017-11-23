c7ebc10e3ac84c34e7986ddfb917a0d008d588b1
James Munnelly <james.munnelly@jetstack.io>
2017-11-23 10:50:07 +0000

    rm infinitely recursive symlink in etcd

--- hack/update-bazel.sh
+++ hack/update-bazel.sh
@@ -8,5 +8,7 @@ set -o pipefail
 # This BUILD file doesn't work when vendored into other projects so we remove
 # it. This causes gazelle to generate a new BUILD file from scratch
 rm vendor/k8s.io/apimachinery/pkg/util/sets/BUILD
+# Remove infinitely recursive symlink from coreos/etcd
+rm vendor/github.com/coreos/etcd/cmd/etcd
 
 bazel run //:gazelle
